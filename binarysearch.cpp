23f297a5795791d4f028433f04c627fa919f2b23
Bianca <bianca.rc@hotmail.com>
2017-04-13 15:35:14 -0300

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,29 +1,9 @@
 #include <iostream>
 
-using std::cin;
-using std::cout;
-using std::endl;
-
 bool binarySearch (int *v, int x, int inicio, int fim) {
 	if (fim < inicio) return false;
 	int k = (fim + inicio)/2;
 	if (v[k] == x) return true;
 	if (v[k] < x) return binarySearch(v, x, k+1, fim);
 	return binarySearch(v, x, inicio, k-1);
-}
-
-int main () {
-	int v[] = {1, 4, 7, 8, 10, 15};
-	int x;
-
-	cout << "Digite um valor inteiro: " << endl;
-	cin >> x;
-
-	if (binarySearch(v, x, 0, 5) == true) {
-		cout << "Inteiro encontrado!" << endl;
-	} else {
-		cout << "Inteiro não encontrado!" << endl;
-	}
-	
-	return 0;
 }
