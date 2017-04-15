d360af828463233ac42f911ffae81f42d06ac935
Bianca <bianca.rc@hotmail.com>
2017-04-14 21:55:49 -0300

    correção quicksort

--- src/quicksort.cpp
+++ src/quicksort.cpp
@@ -6,6 +6,20 @@ void quickSort (int *v, int left, int right) {
 	int tmp;
 	int pivot = v[(left + right) / 2];
 
+	while (i <= j) {
+		while (v[i] < pivot)
+			i++;
+		while (v[j] > pivot)
+			j--;
+		if (i <= j) {
+			tmp = v[i];
+			v[i] = v[j];
+			v[j] = tmp;
+			i++;
+			j--;
+		}
+	}
+
 	if (left < j)
 		quickSort(v, left, j);
 	if (i < right)
