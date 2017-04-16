967e36e41e5f78132b121e2e60f4b31bbd834d8d
Bianca <bianca.rc@hotmail.com>
2017-04-16 06:54:52 -0300

    gráficos (consertar)

---
+++ graph4.p
@@ -0,0 +1,20 @@
+clear
+reset
+set key off
+
+set title "Busca Binária Iterativa"
+
+set autoscale
+
+set xlabel "tamanho da base de busca"
+set ylabel "média do tempo de execução (em ms)"
+
+set xtic auto
+set ytic auto
+
+set terminal png size 640,480 enhanced font "Helvetica 12"
+set output "graph4.png"
+
+set style textbox transparent margins 1.0, 1.0 border
+
+plot "binariaI.dat" using 1:2 title 'Beam' with line
--- src/main.cpp
+++ src/main.cpp
@@ -7,56 +7,50 @@
 using namespace std;
 
 int main () {
-	
 	ofstream binR ("binariaR.dat", ios::app);
 	ofstream binI ("binariaI.dat", ios::app);
 	ofstream seqR ("sequencialR.dat", ios::app);
 	ofstream seqI ("sequencialI.dat", ios::app);
 
-	int n;
+	for (int n = 0; n <= 5000; n = n + 100) {
+		double mediaBinR = 0;
+		double mediaBinI = 0;
+		double mediaSeqR = 0;
+		double mediaSeqI = 0;
 
-	double mediaBinR = 0;
-	double mediaBinI = 0;
-	double mediaSeqR = 0;
-	double mediaSeqI = 0;
+		int v[n];
 
-	cout << "Digite o tamanho do vetor: ";
-	cin >> n;
+		srand(time(NULL));
 
-	int v[n];
+		int x = rand() % 100;
 
-	srand(time(NULL));
+		for (int i = 0; i < n; i++) {
+			v[i] = rand() % 100;
+		}
 
-	int x = rand() % 100;
+		quickSort (v, 0, n);
 
-	for (int i=0; i<n; i++) {
-		v[i] = rand() % 100;
+		for (int i = 0; i < 10; i++) {
+			mediaBinR += binarySearch (v, x, 0, n);
+			mediaBinI += i_binary (x, v, 0, n);
+			mediaSeqR += linearSearch (v, x, n);
+			mediaSeqI += i_linear (v, x, n);
+		}
+
+		mediaBinR = mediaBinR/10;
+		mediaBinI = mediaBinI/10;
+		mediaSeqR = mediaSeqR/10;
+		mediaSeqI = mediaSeqI/10;
+
+		binR << n << " " << mediaBinR << endl;
+		binI << n << " " << mediaBinI << endl;
+		seqR << n << " " << mediaSeqR << endl;
+		seqI << n << " " << mediaSeqI << endl;
 	}
 
-	quickSort (v, 0, n);
-
-	for (int i = 0; i < 10; i++) {
-		mediaBinR += binarySearch (v, x, 0, n);
-		mediaBinI += i_binary (x, v, 0, n);
-		mediaSeqR += linearSearch (v, x, n);
-		mediaSeqI += i_linear (v, x, n);
-	}
-
-	mediaBinR = mediaBinR/10;
-	mediaBinI = mediaBinI/10;
-	mediaSeqR = mediaSeqR/10;
-	mediaSeqI = mediaSeqI/10;
-
-	binR << n << " " << mediaBinR << endl;
 	binR.close();
-
-	binI << n << " " << mediaBinI << endl;
 	binI.close();
-
-	seqR << n << " " << mediaSeqR << endl;
 	seqR.close();
-
-	seqI << n << " " << mediaSeqI << endl;
 	seqI.close();
 
 	return 0;
